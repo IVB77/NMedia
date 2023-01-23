aa9f05306f2af7ba791ec12f63f0c0c15cdc72e6
Vladimir Ivshin <ivshinvb@mail.ru>
2023-01-23 20:24:04 +0500

    Pushes

--- app/src/main/kotlin/ru/netology/pusher/constants.kt
+++
@@ -1,4 +0,0 @@
-package ru.netology.pusher
-
-const val dbUrl = ""
-const val token = "eQARLq2HSIe85_7nO8fboD:APA91bF4GifYPYK9_OeqEJD4gKHDqRw6tvg2tsC6sx0Bu2KROUlxY--FkVUuWCoKqzIqtKdtYhubdnvGGpCdFWnpLtBkHRFqdpiGYC-QlnBq-CvIdXT7iHWeGOSs5QaCZu1PTpKnBL81"
